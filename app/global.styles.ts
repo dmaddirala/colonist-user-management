8c534e101ced69a15d39b0dff1c0722f4ea83a5f
cw-dhruv <88043068+cw-dhruv@users.noreply.github.com>
2025-03-11 01:05:49 +0530

    User List Lazy loading added

--- app/global.styles.ts
+++ app/global.styles.ts
@@ -7,6 +7,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.others.white,
   },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+  },
   filterContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -44,8 +51,13 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     color: Colors.others.black100,
   },
-  listContent: {
-    paddingBottom: 60
+  userIconContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+    marginLeft: 8,
+    borderRadius: 12,
+    backgroundColor: Colors.others.blue100,
   },
   userCard: {
     marginBottom: 12,
@@ -59,6 +71,56 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 6,
   },
+  userRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1
+  },
+  userInfo: {
+    paddingVertical: 16,
+    marginLeft: 10,
+  },
+  userName: {
+    fontWeight: "bold",
+    fontSize: 14,
+    color: Colors.others.black100,
+  },
+  userLine: {
+    width: 4,
+    height: "100%",
+  },
+  note: {
+    marginLeft: 'auto',
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  metaData: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 4,
+  },
+  countryContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+    backgroundColor: Colors.others.blue100,
+    borderRadius: 10,
+    marginRight: 8,
+  },
+  country: {
+    fontSize: 11,
+    fontWeight: "600",
+    color: Colors.others.blue700,
+  },
+  date: {
+    fontSize: 11,
+    fontWeight: "500",
+    marginLeft: 4,
+    color: Colors.others.gray300,
+  },
   cardShadow: {
     overflow: "hidden",
     shadowColor: Colors.others.black100,
@@ -69,6 +131,74 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 6,
   },
+  listContent: {
+    paddingBottom: 60
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    padding: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: Colors.others.black100,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: Colors.others.gray200,
+    borderRadius: 8,
+    padding: 10,
+    minHeight: 80,
+    textAlignVertical: "top",
+    fontSize: 16,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+  },
+  saveButton: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cancelButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: Colors.others.blue600,
+  },
+  cancelText: {
+    color: Colors.others.gray300,
+  }
 });
 
 export default styles;
--- components/ui/UserList.tsx
+++ components/ui/UserList.tsx
@@ -1,9 +1,27 @@
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View, Text, ActivityIndicator, ViewToken, TouchableOpacity } from "react-native";
 import styles from "@/app/global.styles";
 import { UserListProps } from "@/interfaces/UserListProps";
 import { ListEmptyComponent } from "./ListEmptyComponent";
+import { useCallback, useRef } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather } from "@expo/vector-icons";
+import { format } from "date-fns";
+import { Colors } from "@/constants/Colors";
 
-const UserList: React.FC<UserListProps> = ({ users, loading }) => {
+const UserList: React.FC<UserListProps> = ({ users, loading, loadMoreUsers }) => {
+
+  const viewabilityConfig = useRef({ viewAreaCoveragePercentThreshold: 50 });
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems.length > 0) {
+        const lastItem = users[users.length - 1];
+        if (lastItem && viewableItems.some((item) => item.item.id === lastItem.id)) {
+          loadMoreUsers();
+        }
+      }
+    },
+    [users, loadMoreUsers]
+  );
 
   return (
     <>
@@ -14,10 +32,33 @@ const UserList: React.FC<UserListProps> = ({ users, loading }) => {
         contentContainerStyle={styles.listContent}
         renderItem={({ item }) => (
           <View style={[styles.userCard, styles.cardShadow]}>
-            <Text>{item.userName}</Text>
+            <View style={styles.userRow}>
+              <LinearGradient colors={[Colors.others.blue400, Colors.others.purple300]} style={styles.userLine} />
+              <LinearGradient
+                colors={[Colors.others.blue50, Colors.others.purple100]}
+                style={styles.userIconContainer}
+                start={{ x: 0, y: 0.5 }}
+                end={{ x: 0.5, y: 1 }}
+              >
+                <Feather name="user" size={24} color={Colors.others.blue600} />
+              </LinearGradient>
+              <View style={styles.userInfo}>
+                <Text style={styles.userName}>@{item.userName}</Text>
+                <View style={styles.metaData}>
+                  <View style={styles.countryContainer}>
+                    <Text style={styles.country}>{item.country}</Text>
+                  </View>
+                  <Feather name="calendar" size={12} color={Colors.others.gray300} />
+                  <Text style={styles.date}>{format(new Date(item.createdAt), "MM/dd/yyyy")}</Text>
+                </View>
+              </View>
+            </View>
           </View>
         )}
         ListEmptyComponent={loading ? <View /> : <ListEmptyComponent />}
+        ListFooterComponent={loading ? <ActivityIndicator size="large" color={Colors.others.blue700} /> : null}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={viewabilityConfig.current}
       />
     </>
   );
